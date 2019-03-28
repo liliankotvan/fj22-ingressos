375212314a0b600f7ca63f190d8ff087ad6e9bc6
Lilian Kotvan <liliankotvan@gmail.com>
2019-03-28 14:37:45 -0300

    implementando a seleção de lugares, ingressos e tipos de ingressos

--- src/test/java/ingresso/model/descontos/DescontoTest.java
+++ src/test/java/ingresso/model/descontos/DescontoTest.java
@@ -10,28 +10,31 @@ import org.junit.Test;
 
 import br.com.caelum.ingresso.model.Filme;
 import br.com.caelum.ingresso.model.Ingresso;
+import br.com.caelum.ingresso.model.Lugar;
 import br.com.caelum.ingresso.model.Sala;
 import br.com.caelum.ingresso.model.Sessao;
+import br.com.caelum.ingresso.model.TipoDeIngresso;
 import br.com.caelum.ingresso.model.descontos.DescontoParaBancos;
 import br.com.caelum.ingresso.model.descontos.DescontoParaEstudantes;
-import br.com.caelum.ingresso.model.descontos.SemDesconto;
 
 public class DescontoTest {
 	private Sala sala;
 	private Filme filme;
 	private Sessao sessao;
+	private Lugar lugar;
 
 	@Before
 	public void preparaDesconto() {
 		sala = new Sala("Eldorado - IMAX", new BigDecimal("20.5"));
 		filme = new Filme("Rogue One", Duration.ofMinutes(120), "SCI_FI", new BigDecimal("12"));
 		sessao = new Sessao(LocalTime.parse("10:00:00"), filme, sala);
+		lugar = new Lugar("A",1);
 	}
 
 	@Test
 	public void naoDeveConcederDescontoParaIngressoNormal() {
-
-		Ingresso ingresso = new Ingresso(sessao, new SemDesconto());
+		
+		Ingresso ingresso = new Ingresso(sessao, TipoDeIngresso.INTEIRO, lugar);
 
 		BigDecimal precoEsperado = new BigDecimal("32.50");
 		Assert.assertEquals(precoEsperado, ingresso.getPreco());
@@ -40,7 +43,7 @@ public class DescontoTest {
 	@Test
 	public void deveConcederDescontoDe30PorCentoParaIngressosDeClientesDeBancos() {
 
-		Ingresso ingresso = new Ingresso(sessao, new DescontoParaBancos());
+		Ingresso ingresso = new Ingresso(sessao, TipoDeIngresso.BANCO, lugar);
 
 		BigDecimal precoEsperado = new BigDecimal("22.75");
 		Assert.assertEquals(precoEsperado, ingresso.getPreco());
@@ -49,7 +52,7 @@ public class DescontoTest {
 	@Test
 	public void deveConcederDescontoDe50PorCentoParaIngressoDeEstudante() {
 
-		Ingresso ingresso = new Ingresso(sessao, new DescontoParaEstudantes());
+		Ingresso ingresso = new Ingresso(sessao, TipoDeIngresso.ESTUDANTE, lugar);
 
 		BigDecimal precoEsperado = new BigDecimal("16.25");
 		Assert.assertEquals(precoEsperado, ingresso.getPreco());
